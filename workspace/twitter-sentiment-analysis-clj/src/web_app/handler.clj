40a2738a317a2ac5b7a14a783373bd0304bfc21b
DunjaKlisic <klisicdunja@hotmail.com>
2016-03-16 20:36:48 +0100

    Views, handler and css added

---
+++ workspace/twitter-sentiment-analysis-clj/src/web_app/handler.clj
@@ -0,0 +1,28 @@
+(ns web_app.handler
+  (:require [web_app.views :as views]
+            [compojure.core :as cc]
+            [compojure.handler :as handler]
+            [compojure.route :as route]
+            [ring.adapter.jetty :as jetty])
+  (:gen-class))
+
+(cc/defroutes app-routes
+  (cc/GET "/"
+       []
+       (views/home-page))
+  (cc/POST "/classify-tweet"
+        {params :params}
+        (views/classified-tweet-results-page params))
+  (route/resources "/")
+  (route/not-found "Not Found"))
+
+(def app
+  (handler/site app-routes))
+
+(defn -main
+  [& [port]]
+  (let [port (Integer. (or port
+                           (System/getenv "PORT")
+                           5000))]
+    (jetty/run-jetty #'app {:port  port
+                            :join? false})))
---
+++ workspace/twitter-sentiment-analysis-clj/src/web_app/views.clj
@@ -0,0 +1,36 @@
+(ns web_app.views
+(:require   [clojure.string :as str]
+            [hiccup.page :as hic-p]
+            [twitter_sentiment_analysis.data_analysis :as da]))
+
+(defn gen-page-head
+  [title]
+  [:head
+   [:title (str "Twitter sentiment analysis: " title)]
+   (hic-p/include-css "/css/style.css" "/css/reset.css")])
+
+(defn home-page
+  []
+  (hic-p/html5
+   (gen-page-head "Classify a tweet")
+   [:div {:class "pen-title"}
+    [:h1 "Twitter sentiment analysis"]]
+   [:div {:class "module form-module"}
+    [:div {:class "form" :style "display:block"}
+	   [:h2 "Please insert text of a status that you wish to classify"]
+	   [:form {:action "/classify-tweet" :method "POST"} 
+	    [:input {:type "text" :name "tweet" :placeholder "Insert text for classification"}]
+	    [:button "Classify"]]]]))
+
+(defn classified-tweet-results-page
+  [{:keys [tweet]}]
+  (let [sentiment (da/get-sentiment "classifier.txt" tweet)]
+    (hic-p/html5
+     (gen-page-head "Result")
+     [:div {:class "pen-title"} [:h1 "Twitter sentiment analysis"]]
+     [:div {:class "module form-module"}
+      [:div {:class "form" :style "display:block"}
+       [:h2 "This tweet expresses  "sentiment" emotions"]]
+      [:div {:class "cta"} [:a {:href "/"} "Try again"]]])))
+
+
